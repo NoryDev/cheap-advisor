b0f2a79457faef2a0b42d4d763e0dc002b43125a
Fabien Loup <info@norydev.com>
2016-11-22 10:53:17 +0100

    crud operations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.destroy_all
+
+Restaurant.create(name: "Chez Leon", rating: 4, address: "Rue de la fourche")
+Restaurant.create(name: "Quick", rating: 2, address: "Port de namur")
+Restaurant.create(name: "Burger King", rating: 1, address: "Gare du nord")
